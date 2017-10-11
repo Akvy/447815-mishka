037ec664b461a7a8fc33847a40a684aff159fd25
Akvy <a.k.volodchenko@gmail.com>
2017-10-12 01:58:50 +0200

    finished all styles + modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var trendToggle = document.querySelector('.trending__button');
+var trendModal = document.querySelector('.modal-cart');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -13,12 +15,30 @@ navToggle.addEventListener('click', function() {
   }
 });
 
+trendToggle.addEventListener('click', function() {
+  if (trendModal.classList.contains('modal-cart--closed')) {
+    trendModal.classList.remove('modal-cart--closed');
+    trendModal.classList.add('modal-cart--opened');
+  } else {
+    trendModal.classList.add('modal-cart--closed');
+    trendModal.classList.remove('modal-cart--opened');
+  }
+});
 
-// window.addEventListener("keydown", function(evt) {
-//   if (evt.keyCode === 27) {
-//     if (navToggle.classList.contains("main-nav__toggle--opened")) {
-//       navToggle.classList.remove("main-nav__toggle--opened");
-//       navToggle.classList.add("main-nav__toggle--closed");
-//     }
-//   }
-// }
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (trendModal.classList.contains("modal-cart--opened")) {
+      trendModal.classList.remove("modal-cart--opened");
+      trendModal.classList.add("modal-cart--closed");
+    }
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (navToggle.classList.contains("main-nav__toggle--opened")) {
+      navToggle.classList.remove("main-nav__toggle--opened");
+      navToggle.classList.add("main-nav__toggle--closed");
+    }
+  }
+});
